50933592bc826ef4703c158bd822281edda8dfd7
unknown <ducquan7850@gmail.com>
2019-04-08 11:17:10 +0700

    abnormal

--- send.py
+++ send.py
@@ -1,5 +1,5 @@
 import requests
-url = 'http://10.5.37.84:4567/quannd/'
+url = 'http://10.5.37.84:4567/quannd'
 files = {'image': open('1.PNG', 'rb')}
 r=requests.post(url, files=files)
 print(r.text)
